260080320aab7e7ab2992278387d9d9683a68a34
MagNott <notteghem.magali@gmail.com>
2025-05-27 20:20:52 +0200

    #6 Models OK

---
+++ litrevu/reviews/models/ticket_model.py
@@ -0,0 +1,20 @@
+from django.db import models
+# from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
+from PIL import Image
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image:
+            img = Image.open(self.image.path)
+            img.thumbnail((800, 800))  # taille max
+            img.save(self.image.path)
